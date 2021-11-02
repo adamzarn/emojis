02d6188c169812998423f14bc4b5dd7164de3361
Adam Zarn <ajzarn@gmail.com>
2021-11-01 19:52:04 -0500

    Create project

---
+++ Emojis/Emojis/EmojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojisApp.swift
+//  Emojis
+//
+//  Created by Adam Zarn on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
